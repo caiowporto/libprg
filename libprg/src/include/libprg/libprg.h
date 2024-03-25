e6ddcc70f234e90c40b34d976dd2f5312b27b3b1
Caio Porto <caio.hwp2005@aluno.ifsc.edu.br>
2024-03-25 11:28:38 -0300

    .

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -1,6 +1,12 @@
 #ifndef LIBPRG_LIBPRG_H
 #define LIBPRG_LIBPRG_H
 
+#include <libprg/libprg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
 typedef struct lista lista_t;
 
 
@@ -8,9 +14,34 @@ typedef struct lista lista_t;
 int criar_lista(lista_t *lista);
 
 // função inserir
-int inserir_lista()
+int inserir_lista();
 
 
+
+
+typedef struct fila fila_t;
+
+//enfileirar - Adiciona um elemento no final da fila
+fila_t enqueue(int capacidade);
+
+//desenfileirar - Remove o elemento que está no início da fila
+int dequeue(fila_t* f);
+
+//início - Retorna o elemento que está no início da fila
+int head(fila_t) {
+};
+
+//fim - Retorna o elemento que está no final da fila
+int tail(fila_t);
+
+//tamanho - Retorna o total de elementos na fila
+int size();
+
+//vazia - Indica se a fila está vazia ou não
+int empty(fila_t* f);
+
+//cheia - Indica se a fila está cheia ou não
+int full();
 
 
 
---
+++ libprg/src/libprg/lista_contato.c
@@ -0,0 +1,16 @@
+#include "libprg/libprg.h"
+
+//
+// Created by aluno on 25/03/24.
+//
+int N;
+
+struct fila {
+    int inicio, fim;
+    float vet[N];
+};
+
+fila_t enqueue (int capacidade)
+{
+    if ()
+}
