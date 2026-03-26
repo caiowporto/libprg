7af27e000a16d849a57b6a4b6a41a9fd0c52c3df
Caio Porto <caio.hwp2005@aluno.ifsc.edu.br>
2026-03-26 17:35:20 -0300

    Iniciada a implementação da fila. Funções criar_fila, destruir_fila e enfileirar criadas.

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -1,7 +1,16 @@
 #ifndef LIBPRG_LIBPRG_H
 #define LIBPRG_LIBPRG_H
 
+// === PILHA ===
+
 typedef struct pilha pilha_t;
 pilha_t* criar_pilha(int capacidade);
+
+// === FILA ===
+
+typedef struct fila fila_t;
+fila_t* criar_fila(int capacidade);
+void enfileirar(fila_t* fila, int valor);
+int destruir_fila(fila_t* fila);
 
 #endif
