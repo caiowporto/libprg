cccfd5fa5b2d7f65f6d426697eed6fadd53ec59c
caiowporto <141061237+caiowporto@users.noreply.github.com>
2026-06-10 22:25:46 -0300

    Funções de busca e remover nó na árvore foram adicionadas.

--- libprg/src/libprg/arvore.c
+++ libprg/src/libprg/arvore.c
@@ -61,3 +61,58 @@ void travessia_posordem(noa_t* raiz) {
         printf("%d ", raiz->dado);
     }
 }
+
+noa_t* remover_valor(noa_t* raiz, int dado) {
+    if (raiz == NULL) return raiz;
+    if (valor < raiz->dado) {
+        raiz->esquerda = remover_valor(raiz->esquerda, dado);
+    } else if (valor > raiz->dado) {
+        raiz->direita = remover_valor(raiz->direita, dado);
+    } else {
+        // IF nó folha ou nó com um filho
+        if (raiz->esquerda == NULL) {
+            noa_t* temp = raiz->direita;
+            free(raiz);
+            return temp;
+        } else if (raiz->direita == NULL) {
+            noa_t* temp = raiz->esquerda;
+            free(raiz);
+            return temp;
+        }
+        // ELSE nó com dois filhos
+        else {
+            // Encontra o menor nó da direita direto aqui dentro
+            noa_t* atual = raiz->direita;
+            while (atual->esquerda != NULL) {
+                atual = atual->esquerda;
+            }
+            // Copia o valor do menor da direita para o nó atual
+            raiz->dado = atual->dado;                
+            // Remove o nó copiado da subárvore direita
+            raiz->direita = remover_valor(raiz->direita, atual->dado);
+        }
+    }
+    return raiz;
+}
+
+bool busca(noa_t* raiz, int dado) {
+    if (raiz == NULL) return false;
+    if (dado == raiz->dado) return true;
+    if (dado < raiz->dado) return busca(raiz->esquerda, dado);
+    return busca(raiz->direita, dado);
+}
+
+bool busca_it(noa_t* raiz, int dado) {
+    noa_t* atual = raiz;
+    while (atual != NULL) {
+        if (dado == atual->dado) {
+            return true;
+        }
+        if (dado < atual->dado) {
+            atual = atual->esquerda;
+        } else {
+            atual = atual->direita;
+        }
+    }
+    return false;
+}
